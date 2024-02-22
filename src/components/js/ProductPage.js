6521dc21b13e8071c99f76a7d90e72c2f17b28e3
sameoldcarlos <albertocarlos221@gmail.com>
2024-02-21 22:06:35 -0300

    feat: product page

---
+++ src/components/js/ProductPage.js
@@ -0,0 +1,9 @@
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
---
+++ src/views/ProductPageView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ProductPage from '@/components/ProductPage.vue'
+</script>
+
+<template>
+  <main>
+    <product-page />
+  </main>
+</template>
