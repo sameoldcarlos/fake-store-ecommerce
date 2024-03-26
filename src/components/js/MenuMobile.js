1d6179a05b1b56e2dd693d96cf042a11d5bbf662
sameoldcarlos <albertocarlos221@gmail.com>
2024-03-26 01:46:07 -0300

    fix: refactor MenuMobile.js to use pinia and language store

--- src/components/js/MenuMobile.js
+++ src/components/js/MenuMobile.js
@@ -1,6 +1,7 @@
 import { categories } from "@/utils/content.js"
 
-const MENU_OPTIONS = categories.filter(item => item.value !== 'all').map(item => ({ ...item, link: `/products/${item.value}` }))
+import { mapState } from 'pinia'
+import { useLanguageStore } from '@/stores/language'
 
 export default {
   props: {
@@ -10,9 +11,18 @@ export default {
     }
   },
 
-  data() {
-    return {
-      menuOptions: MENU_OPTIONS
+  inject: ['appTextData'],
+
+  computed: {
+    ...mapState(useLanguageStore, ['selectedLanguage']),
+
+    textContent() {
+      return this.appTextData[this.selectedLanguage]
+    },
+
+    menuOptions() {
+      const { textContent } = this
+      return categories.filter(item => item.value !== 'all').map(item => ({ ...item, label: textContent[item.value], link: `/products/${item.value}` }))
     }
   },
 
